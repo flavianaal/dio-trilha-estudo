ac9618157606560ddf66ce62c2f8ad46477caad9
Flaviana Andrade <flavianaandradedelima@gmail.com>
2023-03-02 16:23:03 -0300

    Atualizando

--- TipoVariaveis/src/TipoVariaveis.java
+++ TipoVariaveis/src/TipoVariaveis.java
@@ -1,5 +1,14 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // short numeroCurto = 1;
+        // int numeroNormal2 = numeroCurto;
+        // short numeroCurto1= (short) numeroNormal2;
+
+        // int numero = 1;
+        // numero = 2;
+        // tipos primitivos
+        // estudem a classe String que representa textos
+        // operadores = atribuir valores a uma variavel
+
     }
 }
